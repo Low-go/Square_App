383979ad425653eb354f33b1367f5d465c0e123b
Low-go <lorranalvesgaldino98@gmail.com>
2025-02-26 16:58:06 -1000

    limit to portrait mode. Create a test square, will need to find correct height and width

--- app/src/main/java/com/example/square_app/MainView.java
+++ app/src/main/java/com/example/square_app/MainView.java
@@ -1,20 +1,27 @@
 package com.example.square_app;
 
 import android.graphics.Color;
+import android.graphics.Paint;
 import android.view.View;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.util.Log;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainView extends View {
     private float screenWidth, screenHeight;
     public boolean gameStarted;
+
+    private List<NumberedSquare> squares;
+    private NumberedSquare testSquare;
 
     // We will be making this game in portait mode
     public MainView(Context c){
         super(c);
         gameStarted = false;
-
+        squares = new ArrayList<>();
 
     }
 
@@ -29,7 +36,10 @@ public class MainView extends View {
             gameStarted = true;
             Log.d("test", "Screen Width: " + screenWidth);;
             Log.d("test", "Screen Height: " + screenHeight);
+            testSquare = new NumberedSquare(screenWidth, screenHeight);
         }
+
+        testSquare.draw(c);
     }
 
 }
--- app/src/main/java/com/example/square_app/NumberedSquare.java
+++ app/src/main/java/com/example/square_app/NumberedSquare.java
@@ -1,17 +1,36 @@
 package com.example.square_app;
 
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.RectF;
 
 public class NumberedSquare {
 
-    private float screenWidth, screenHeight;
+    private float squareWidth, squareHeight;
 
-    public NumberedSquare(){
+    private Paint paint;
+    private RectF bounds;
 
+    public NumberedSquare(float w, float h){
+
+        paint = new Paint();
+        paint.setColor(Color.WHITE);
+
+        //10 percent of screen size
+        squareWidth = w * 0.1f;
+        squareHeight = h * 0.1f;
+
+        // random x y positions on map
+        float x = (float) (Math.random() * w);
+        float y = (float) (Math.random() * h);
+
+        bounds = new RectF( x, y, squareWidth, squareHeight);
     }
 
 
     public void draw(Canvas c){
 
+        c.drawRect(bounds, paint);
     }
 }
