d7670531fbef1b42220bf5481650c9a316c1dec1
Low-go <lorranalvesgaldino98@gmail.com>
2025-02-27 00:23:15 -1000

    add number in middle, make outline, testsquare is finished. Need to make 5 of them now

--- app/src/main/java/com/example/square_app/NumberedSquare.java
+++ app/src/main/java/com/example/square_app/NumberedSquare.java
@@ -4,25 +4,37 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.RectF;
+import android.graphics.Typeface;
 
 public class NumberedSquare {
 
     private float squareWidth, squareHeight;
 
-    private Paint paint;
+    private Paint outlinePaint;
+    private Paint textPaint;
     private RectF bounds;
 
     public NumberedSquare(float w, float h){
 
-        paint = new Paint();
-        paint.setColor(Color.WHITE);
+        // this is for the outline
+        outlinePaint = new Paint();
+        outlinePaint.setColor(Color.WHITE);
+        outlinePaint.setStyle(Paint.Style.STROKE);
+        outlinePaint.setStrokeWidth(10f);
+
+
+        // this is for the text in the center
+        textPaint = new Paint();
+        textPaint.setColor(Color.BLACK);
+        textPaint.setTextSize(50f);
+        textPaint.setTypeface(Typeface.DEFAULT_BOLD); // lets see if this typeface stuff does anything
 
         //10 percent of screen size
-        squareWidth = w * 0.2f;
-        squareHeight = h * 0.13f;
+        squareWidth = w * 0.25f;
+        squareHeight = h * 0.15f;
 
         // random x y positions on map
-        float x = (float) (Math.random() * (w - squareWidth));
+        float x = (float) (Math.random() * (w - squareWidth)); // we subtract the squares size to make sure it does not get out of bounds
         float y = (float) (Math.random() * (h - squareHeight));
 
         bounds = new RectF(x , y, x + squareWidth, y +  squareHeight);
@@ -32,6 +44,7 @@ public class NumberedSquare {
 
     public void draw(Canvas c){
 
-        c.drawRect(bounds, paint);
+        c.drawRect(bounds, outlinePaint);
+        c.drawText("5", bounds.centerX() - 6f, bounds.centerY() + 1f, textPaint);
     }
 }
